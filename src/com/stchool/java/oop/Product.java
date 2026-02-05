e779ae343eaaad7c815be7a8a7f27769705ffe90
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-02-05 22:25:05 +0530

    feat: Add product methods

--- src/com/stchool/java/oop/Product.java
+++ src/com/stchool/java/oop/Product.java
@@ -1,50 +1,72 @@
 package com.stchool.java.oop;
 
 public class Product {
-    //instance variable
-    int id,quantity,availableStock, shippingCharge;
-    int price;
-    char memberShip;
+    int id;
     String name;
-    float stateTax,centralTax,discountAmount,totalPrice,discountPercentage;
-    float stateTaxPercentage;
-    float centralTaxPercentage;
-    Product(){
-        System.out.println("Creating a product ");
+    float maxRetailPrice;
+    float centralTax;
+    float stateTax;
+    float discountPercentage;
+    float discountAmount;
+    float finalPrice;
+    int shippingCharges;
+
+    public Product() {
+        id = 123;
+        name = "dell laptop";
+        maxRetailPrice = 50000.00F;
+        centralTax = 2.5F;
+        stateTax = 2.5F;
+        discountPercentage = 7.5F;
+        shippingCharges=100;
 
     }
 
-    public Product(int id, int quantity, int availableStock, int shippingCharge, int price, char memberShip, String name, float stateTax, float centralTax, float discountAmount, float totalPrice, float discountPercentage, float stateTaxPercentage, float centralTaxPercentage) {
+    public Product(int id, String name, float maxRetailPrice, float centralTax, float stateTax, float discountPercentage, float discountAmount, float finalPrice, int shippingCharges) {
         this.id = id;
-        this.quantity = quantity;
-        this.availableStock = availableStock;
-        this.shippingCharge = shippingCharge;
-        this.price = price;
-        this.memberShip = memberShip;
         this.name = name;
-        this.stateTax = stateTax;
+        this.maxRetailPrice = maxRetailPrice;
         this.centralTax = centralTax;
-        this.discountAmount = discountAmount;
-        this.totalPrice = totalPrice;
+        this.stateTax = stateTax;
         this.discountPercentage = discountPercentage;
-        this.stateTaxPercentage = stateTaxPercentage;
-        this.centralTaxPercentage = centralTaxPercentage;
+        this.discountAmount = discountAmount;
+        this.finalPrice = finalPrice;
+        this.shippingCharges = shippingCharges;
     }
 
-    void displayProducts() {
-        System.out.println("name: " + name);
-        System.out.println("Id: " + id);
-        System.out.println("quantity: " + quantity);
-        System.out.println("shippingCharge: " + shippingCharge);
-        System.out.println("availableStock: " + availableStock);
-        System.out.println("membership: " + memberShip);
-        System.out.println("price: " + price);
-        System.out.println("StateTax: " + stateTax);
-        System.out.println("CentralTax: " + centralTax);
-        System.out.println("discountAmount: " + discountAmount);
-        System.out.println("TotalPrice: " + totalPrice);
-        System.out.println("discountPercentage: " + discountPercentage);
-        System.out.println("stateTaxPercentage: " + stateTaxPercentage);
-        System.out.println("centalTaxPercentage: " + centralTaxPercentage);
+    public void displayProduct() {
+        System.out.println("Product ID : " + id);
+        System.out.println("Product Name : " + name);
+        System.out.println("Product maxRetailPrice : " + maxRetailPrice);
+        System.out.println("Discount Amount : "+calculateDiscount());
+        System.out.println("Price after Discount : "+priceAfterDiscount());
+        System.out.println("centralTax : " + centralTax);
+        System.out.println("StateTax : " + stateTax);
+        System.out.println("Shipping charges : " + shippingCharges);
+        System.out.println("Final Bill : "+calculateFinalPrice());
+
+    }
+
+    float calculateDiscount() {
+        return maxRetailPrice * discountPercentage / 100;
+
+    }
+
+    float priceAfterDiscount() {
+        return maxRetailPrice - calculateDiscount();
+
+    }
+
+    float calculateStateTax() {
+        return priceAfterDiscount() * stateTax / 100;
+    }
+
+    float calculateCentralTax() {                           //methods
+        return priceAfterDiscount() * centralTax / 100;
+    }
+
+    float calculateFinalPrice() {
+        return priceAfterDiscount()+calculateStateTax()+calculateCentralTax() + shippingCharges;
+
     }
 }
