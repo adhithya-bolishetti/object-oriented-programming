821edd56dc0df495fa03d279ccd7b77b82b38ee9
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-02-05 22:25:52 +0530

    feat: Add product methods invocation

--- src/com/stchool/java/oop/Main.java
+++ src/com/stchool/java/oop/Main.java
@@ -2,50 +2,62 @@ package com.stchool.java.oop;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1;//reference var
+        Product product1;
         product1 = new Product();
-        product1.displayProducts();
-//        System.out.println("name: " + product1.name);
-//        System.out.println("Id: " + product1.id);
-//        System.out.println("quantity: " + product1.quantity);
-//        System.out.println("shippingCharge: " + product1.shippingCharge);
-//        System.out.println("availableStock: " + product1.availableStock);
-//        System.out.println("membership: " + product1.memberShip);
-//        System.out.println("price: " + product1.price);
-//        System.out.println("StateTax: " + product1.stateTax);
-//        System.out.println("CentralTax: " + product1.centralTax);
-//        System.out.println("discountAmount: " + product1.discountAmount);
-//        System.out.println("TotalPrice: " + product1.totalPrice);
-//        System.out.println("discountPercentage: " + product1.discountPercentage);
-//        System.out.println("stateTaxPercentage: " + product1.stateTaxPercentage);
-//        System.out.println("centalTaxPercentage: " + product1.centralTaxPercentage);
-        System.out.println("----------------------------------------------------");
-
-        Product product2;
-        product2 = new Product(2341, 2, 125, 50, 3000, 's', "lenovo", 2.5F, 2.5F, 1000.0F, 40000.0F, 1.5F, 2000.0F, 2000.0F);
-        product2.displayProducts();
-        //        System.out.println("name: " + product2.name);
-//        System.out.println("Id: " + product2.id);
-//        System.out.println("quantity: " + product2.quantity);
-//        System.out.println("shippingCharge: " + product2.shippingCharge);
-//        System.out.println("availableStock: " + product2.availableStock);
-//        System.out.println("membership: " + product2.memberShip);
-//        System.out.println("price: " + product2.price);
-//        System.out.println("StateTax: " + product2.stateTax);
-//        System.out.println("CentralTax: " + product2.centralTax);
-//        System.out.println("discountAmount: " + product2.discountAmount);
-//        System.out.println("TotalPrice: " + product2.totalPrice);
-//        System.out.println("discountPercentage: " + product2.discountPercentage);
-//        System.out.println("stateTaxPercentage: " + product2.stateTaxPercentage);
-//        System.out.println("centalTaxPercentage: " + product2.centralTaxPercentage);
-        System.out.println("---------------------------------------------------");
 
 
-        //table format
-        System.out.format("%-5s %-10s %-10s %-15s %-15s%n",
+        /*System.out.println("----------------------------------------------");
+        System.out.println(" ");
+        System.out.println("Product ID : " + product1.id);
+        System.out.println("Product Name : " + product1.name);
+        System.out.println("Product maxRetailPrice : " + product1.maxRetailPrice);
+        System.out.println("centralTax : " + product1.centralTax);
+        System.out.println("quantity : " + product1.quantity);
+        System.out.println(" ");*/
 
-                "Id", "Price", "Name", "CentralTax", "TotalPrice");
-        System.out.println("-----------------------------------------------");
-        //table formate with data
+        /*System.out.println("-----------------------------------------------------");
+
+        System.out.println(" ");
+        System.out.printf("%-20s %-20d%n", "Product ID         : ", product1.id);
+        System.out.printf("%-20s %-20s%n", "Product Name       : ", product1.name);
+        System.out.printf("%-20s %-20.2f%n", "Max Retail Price   : ", product1.maxRetailPrice);
+        System.out.printf("%-20s %-20.2f%n", "Central Tax        : ", product1.centralTax);
+        System.out.printf("%-20s %-20d%n", "Quantity          : ", product1.quantity);
+        System.out.println(" ");
+
+        System.out.println("-----------------------------------------------------");*/
+
+      /* Product product2 = new Product(1, "hp", 50000.0F, 10, 2.5F);
+        System.out.println("parameterized initialised");
+        System.out.println("----------------------------------------------");
+        System.out.println(" ");
+        System.out.println("Product ID : " + product2.id);
+        System.out.println("Product Name : " + product2.name);
+        System.out.println("Product maxRetailPrice : " + product2.maxRetailPrice);
+        System.out.println("centralTax : " + product2.centralTax);
+        System.out.println("quantity : " + product2.quantity);
+        System.out.println(" ");*/
+
+       /* Product product2 = new Product(1, "hp", 50000.00F, 10, 2.5F,7.5F);
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("parameterized initialised");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.printf("%-10s %-15s %-20s %-10s %-10s%n",
+                "ID", "NAME", "MRP", "QTY", "TAX");
+        System.out.println("--------------------------------------------------------------------------------");
+
+        System.out.printf("%-10d %-15s %-20.2f %-10d %-10.2f%n",
+                product2.id,
+                product2.name,
+                product2.maxRetailPrice,
+                product2.quantity,
+                product2.centralTax);
+
+        System.out.println("--------------------------------------------------------------------------------");*/
+        Product product3=new Product();
+        product3.displayProduct();
+
     }
+
 }
